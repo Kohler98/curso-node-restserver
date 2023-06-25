e988eda2236b0362bc1751fbe25a0279f3c84d49
Kohler98 <109408596+Kohler98@users.noreply.github.com>
2023-06-24 20:49:38 -0400

    default port update

--- models/server.js
+++ models/server.js
@@ -4,7 +4,7 @@ class Server{
     constructor(){
         this.app = express()
         this.usuariosPath = '/api/usuarios'
-        this.PORT = process.env.PORT
+        this.PORT = process.env.PORT || 3000
         
         //Midlewares : no son mas que funciones que van a añadirle otras funcionalidades al web server
         // en otras palabras es una funcion que se ejecuta cuando se levanta el servidor
